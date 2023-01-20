7c730e164d1aba7d1ca500fdf71781c39721468e
Peter D'hondt <dhondt.peter@gmail.com>
2023-01-20 21:51:59 +0100

    taak 1.13 Gezin - class Gezin toegevoegd

---
+++ src/main/java/be/vdab/domain/Gezin.java
@@ -0,0 +1,26 @@
+package be.vdab.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gezin {
+    private final String papa;
+    private final String mama;
+    private final List<String> kinderen = new ArrayList<>();
+    public Gezin(String papa, String mama) {
+        this.papa = papa;
+        this.mama = mama;
+    }
+    public void addKind(String kind) {
+        kinderen.add(kind);
+    }
+    public String getPapa() {
+        return papa;
+    }
+    public String getMama() {
+        return mama;
+    }
+    public List<String> getKinderen() {
+        return kinderen;
+    }
+}
